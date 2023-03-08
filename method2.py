e7d491bf939cb5d4ed4ec942d060d473e4e0e228
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-09 08:47:32 +0900

    -1が返される例も記載しました

--- method2.py
+++ method2.py
@@ -4,6 +4,7 @@ pta = 'Perfume'
 # 最初に現れる位置をインデックスで返す,該当が無ければ-1
 # indexメソッドだと-1ではなくエラー
 print(pta.find('e'))  # 1
+print(pta.find('g'))  # -1
 print('\n')
 # 最後に現れるインデックスを取得(rindexも同様)
 print(pta.rfind('e'))  # 6
