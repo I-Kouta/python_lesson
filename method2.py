0ca67445dab8d85d25692fde83b292ef96c402eb
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-09 20:20:11 +0900

    countメソッドの記述

--- method2.py
+++ method2.py
@@ -9,3 +9,8 @@ print('\n')
 # 最後に現れるインデックスを取得(rindexも同様)
 print(pta.rfind('e'))  # 6
 print(pta.rfind('ume'))  # 4
+print('\n')
+# countメソッド：大文字と小文字の区別アリ
+print('Spinning World'.count('n'))  # 3
+print('Spinning World'.count('s'))  # 0
+print('Spinning World'.count('i', 0, 2))  # Spまでが対象なので0
