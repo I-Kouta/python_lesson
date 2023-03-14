4902c2126654b746c57f10478c1757252720316a
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-14 14:47:30 +0900

    条件分岐の記述

--- if.py
+++ if.py
@@ -1,1 +1,18 @@
 # 条件分岐
+music = "Spring of Life"
+print("春の曲といえば" + music + " 派\n")
+
+if music == "Spring of Life":
+    print("サプライズを待っていてもしょうがないから〜")
+else:
+    print("ワンルーム・ディスコ派ですか？")
+print("ドライブはFAKE IT派です")
+
+likePerfume = "気になる姫"
+if likePerfume == "こどものっち":  # ここは一致を確かめるので==
+    PerfumeLocks = "こどものっち"  # ここは代入なので=
+elif likePerfume == "かしゆかおじさん":
+    PerfumeLocks = "かしゆかおじさん"
+else:
+    PerfumeLocks = "気になる姫"
+print("Perfume Locksといえば" + PerfumeLocks + "!!!")
