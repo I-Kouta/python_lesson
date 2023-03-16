8bfebfd0cc665fd4677a3c778a71f1f0af5f2246
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-16 14:44:53 +0900

    オブジェクトの演算の記述

--- if.py
+++ if.py
@@ -27,10 +27,18 @@ print('\n')
 print(ord("a"))  # 97
 print(ord("B"))  # 66
 # 要素が含まれる
-array = ["レーザービーム", "スパイス", "いじわるなハロー"]
-print(bool("スパイス" in array))  # true
-print(bool("ビーム" in array))  # false
+array1 = ["レーザービーム", "スパイス", "いじわるなハロー"]
+print(bool("スパイス" in array1))  # true
+print(bool("ビーム" in array1))  # false
 # 論理和・論理積・否定
 print(bool(5 < msgA or 20 < msgA))  # true,一方がtrueならtrueを返す
 print(bool(5 < msgA and 20 < msgA))  # false,一方がfalseならfalseを返す
 print(bool(not 5 < msgA))  # 逆を返す
+print("\n")
+# オブジェクトが同一かどうか比較
+array2 = ["レーザービーム", "スパイス", "いじわるなハロー"]
+array3 = array2
+print(array1 == array2)  # true
+print(array1 is array2)  # オブジェクトが別々なのでfalse
+print(array2 is array3)  # 同じオブジェクトを示しているのでtrue
+print("\n")
