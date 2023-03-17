5b2970e88826244715321fe03c28b0e65bdb3acf
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-17 14:49:36 +0900

    説明のコメントの追加です

--- for.py
+++ for.py
@@ -1,11 +1,10 @@
 # 繰り返し処理
-num = 1
+num = 1  # この値から
 sum = 0
 last = 80
 print(str(num) + "から" + str(last) + "までの総和を算出")
 while num <= last:  # いくつまで
-    sum += num**4  # 何を足すか
-    num += 1
-print(sum)
-
-print("end")
+    sum += num**4  # 右辺の処理をして繰り返す
+    num += 1  # 1ずつ増やしていく
+else:
+    print("計算結果は" + str(sum) + "です")
