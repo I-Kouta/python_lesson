63e925b543bb4f91f765e6259469bd991561c66f
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-17 19:15:37 +0900

    繰り返し処理発展

--- for.py
+++ for.py
@@ -8,3 +8,23 @@ while num <= last:  # いくつまで
     num += 1  # 1ずつ増やしていく
 else:
     print("計算結果は" + str(sum) + "です")
+
+# 1から数えて逆数の和が8を越えるのはいくつか
+reciprocal_ans = 0
+reciprocal_n = 0
+while reciprocal_ans < 8:  # これを越える値
+    reciprocal_n += 1
+    reciprocal_ans += 1/reciprocal_n
+print(reciprocal_n)  # めっちゃ大きい
+print(reciprocal_ans)  # ちょっと値を越える
+print(reciprocal_ans - 1 / (reciprocal_n))  # ギリギリ超えない
+
+# 1から数えて逆数の和が8を越えるのはいくつか
+ans = 0
+n = 0
+while ans < 1000:  # これを越える値
+    n += 1
+    ans += n
+print(n)  # めっちゃ大きい
+print(ans)  # ちょっと値を越える
+print(ans - n)  # ギリギリ超えない
