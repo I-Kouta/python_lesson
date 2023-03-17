c03dd50b1b4fbef6fec70a7bcb3c98b3c71308f0
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-17 14:42:33 +0900

    シグマ算の記述

--- for.py
+++ for.py
@@ -1,1 +1,11 @@
 # 繰り返し処理
+num = 1
+sum = 0
+last = 80
+print(str(num) + "から" + str(last) + "までの総和を算出")
+while num <= last:  # いくつまで
+    sum += num**4  # 何を足すか
+    num += 1
+print(sum)
+
+print("end")
