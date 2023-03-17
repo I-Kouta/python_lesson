1f980921546fb00baa84827598bea0bcd6e89f93
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-17 19:44:23 +0900

    計算結果を文章と結合しました

--- while.py
+++ while.py
@@ -1,7 +1,7 @@
 # 繰り返し処理
 num = 1  # この値から
-sum = 0
-last = 44
+last = 45  # この値まで
+sum = 0  # 最初は0からスタート
 print(str(num) + "から" + str(last) + "までの総和を算出")
 while num <= last:  # いくつまで
     sum += num  # 右辺の処理をして繰り返す
@@ -15,16 +15,15 @@ reciprocal_n = 0
 while reciprocal_ans < 8:  # これを越える値
     reciprocal_n += 1
     reciprocal_ans += 1/reciprocal_n
-print(reciprocal_n)  # めっちゃ大きい
-print(reciprocal_ans)  # ちょっと値を越える
-print(reciprocal_ans - 1 / (reciprocal_n))  # ギリギリ超えない
+print(str(reciprocal_n) + "までの逆数の総和は" + str(reciprocal_ans) + "です")
+number = str((reciprocal_ans - 1 / (reciprocal_n)))  # 1行にまとめたいので変数にします
+print(str(reciprocal_n - 1) + "までの逆数の総和は" + number + "となり、ギリギリ超えません")
 
 # 1から数えて和が1000を越えるのはいくつか
 ans = 0
 n = 0
-while ans < 1000:  # これを越える値
+while ans < 100:  # これを越える値
     n += 1
     ans += n
-print(n)  # これを足すと値を越える
-print(ans)  # ちょっと値を越える
-print(ans - n)  # ギリギリ超えない
+print(str(n) + "まで足すと総和は" + str(ans) + "となり、目的の値を超えます")
+print(str(n-1) + "まで足すと総和は" + str(ans-n) + "です")
