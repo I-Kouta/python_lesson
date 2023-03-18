69f13eaabc40c5a61c6ca2ef3a64a2cda7d4724a
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-18 13:39:35 +0900

    記述一部修正です

--- while.py
+++ while.py
@@ -7,7 +7,7 @@ while num <= last:  # いくつまで
     sum += num  # 右辺の処理をして繰り返す
     num += 1  # 1ずつ増やしていく
 else:
-    print("計算結果は" + str(sum) + "です")
+    print("計算結果は" + str(sum) + "です\n")
 
 # 1から数えて逆数の和が8を越えるのはいくつか
 reciprocal_ans = 0
@@ -17,7 +17,7 @@ while reciprocal_ans < 8:  # これを越える値
     reciprocal_ans += 1/reciprocal_n
 print(str(reciprocal_n) + "までの逆数の総和は" + str(reciprocal_ans) + "です")
 number = str((reciprocal_ans - 1 / (reciprocal_n)))  # 1行にまとめたいので変数にします
-print(str(reciprocal_n - 1) + "までの逆数の総和は" + number + "となり、ギリギリ超えません")
+print(str(reciprocal_n - 1) + "までの逆数の総和は" + number + "となり、目的の値に最も近づきます\n")
 
 # 1から数えて和が1000を越えるのはいくつか
 ans = 0
