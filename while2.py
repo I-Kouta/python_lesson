124fe1926d5471db7b5228754d7bf0a70eb86d77
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-20 14:27:29 +0900

    for文での繰り返しの記述

--- while2.py
+++ while2.py
@@ -1,1 +1,14 @@
 # for文を使った繰り返し
+setList = ["edge", "Tiny Baby", "Hurly Burly"]
+for list in setList:
+    print(list)
+else:
+    print("\n")
+
+count = 0
+list = {"e": "edge", "T": "Tiny Baby", "H": "Hurly Burly"}
+for initial, value in list.items():
+    print("頭文字：" + initial + ",曲：" + value)
+    count += 1
+else:
+    print("曲数：" + str(count))
