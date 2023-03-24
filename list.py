594f74988fc663de80585720f40f1800c5a0bc15
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-24 14:38:53 +0900

    リストの要素を取得する記述

--- list.py
+++ list.py
@@ -1,2 +1,7 @@
 # リストを作成する
 PerfumeList = ["Pick Me Up", "Spinning World", "コミュニケーション", "FAKE IT"]
+print(PerfumeList[0])  # Pick Me Up
+print(PerfumeList[-1])  # FAKE IT
+print(PerfumeList[-2])  # コミュニケーション
+print("最後の要素は" + PerfumeList[len(PerfumeList) - 1])
+print("要素の数は" + str(len(PerfumeList)) + "つです")
