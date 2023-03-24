2adb8fc1a83f31272c2f24865e1200b964129be6
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-25 08:03:14 +0900

    スライス機能の記述

--- list.py
+++ list.py
@@ -4,4 +4,9 @@ print(PerfumeList[0])  # Pick Me Up
 print(PerfumeList[-1])  # FAKE IT
 print(PerfumeList[-2])  # コミュニケーション
 print("最後の要素は" + PerfumeList[len(PerfumeList) - 1])
-print("要素の数は" + str(len(PerfumeList)) + "つです")
+print("要素の数は" + str(len(PerfumeList)) + "つです\n")
+
+# スライス機能
+print(PerfumeList[0:2])  # SWまで。最後は要素+1
+print(PerfumeList[:2])  # 同じ
+print(PerfumeList[2:])
