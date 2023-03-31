cdf6524e53025b65eb333dc22fa30e10f7770248
I-Kouta <kouta.baseball0222@gmail.com>
2023-03-31 14:51:21 +0900

    要素を取得する記述

--- tuple.py
+++ tuple.py
@@ -4,3 +4,8 @@ y = 4
 z = 2
 numTuple = (x, y, z)
 print(numTuple)
+# 要素を取得
+print(str(numTuple[0]) + " + " + str(numTuple[1]) + " + " +
+      str(numTuple[2]) + " = " + str(numTuple[0] + numTuple[1] + numTuple[2]))
+print(str(numTuple[0]) + " × " + str(numTuple[1]) + " × " +
+      str(numTuple[2]) + " = " + str(numTuple[0] * numTuple[1] * numTuple[2]))
