03a3fc8e373b7201e7d510340dcb81657d5c1ecd
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-01 11:41:41 +0900

    対象の文字列を探す記述

--- regex.py
+++ regex.py
@@ -1,1 +1,8 @@
 # 正規表現
+import re
+target64 = "495j6t9h4jy59nhjt049ehd8gbhetognbodoj988t7tgubuvcrtdr0j9ihbyvcty"
+# print(len(target64))
+pattern = re.compile(r'n...o')  # n---oがあれば返す
+result = pattern.search(target64)
+if result:
+    print(result.group())
