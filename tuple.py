e65ae5bd9a9e224805e1d61f567b8b3f975c0608
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-01 11:06:22 +0900

    タプルの結合

--- tuple.py
+++ tuple.py
@@ -13,3 +13,6 @@ print(str(numTuple[0]) + " × " + str(numTuple[1]) + " × " +
 print("要素数は" + str(len(numTuple)) + "つです")
 print(str(len(numTuple)) + "つ数値の積は" +
       str(numTuple[0] * numTuple[1] * numTuple[2]) + "です")
+numTupleB = (5, 6)
+newNumTuple = numTuple + numTupleB
+print(newNumTuple)  # (3, 4, 2, 5, 6)
