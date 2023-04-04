1b640c9c7b702e42617b49e3c01e76541cb4e9f5
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-04 14:30:45 +0900

    一致した文字列を返す記述

--- regex2.py
+++ regex2.py
@@ -11,5 +11,17 @@ def checkMatch(msg, pat):
         print("完全一致しません")
 
 
-checkMatch("シークレットシークレット", r"シ.*シ")  # 一致しません
+checkMatch("シークレットシークレット", r"シ.*シ")  # 完全一致しません
 checkMatch("シークレットシークレット", r"シ.*ト")  # シークレットシークレット
+
+birth = "NA-0215, KY-1223, OA-0920"
+pattern2 = re.compile(r"[A-Z]{2}-[0-9]{4}")
+result2 = pattern2.findall(birth)
+
+if (len(result2) == 0):
+    print("一致しません")
+else:
+    for s in result2:
+        print(s)
+
+print(str(len(result2)) + "つ一致する要素があります")
