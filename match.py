07d7b1474c2edb4b3fcada00b47e3283d34cc24a
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-06 14:34:18 +0900

    キャプチャグループにマッチした部分文字列を取得

--- match.py
+++ match.py
@@ -1,1 +1,10 @@
 # マッチオブジェクトからマッチした文字列の情報を取得
+import re
+msg1 = 'birthday is 1989/02/15'
+pat1 = re.compile(r"(\d{4})/(\d{2})/(\d{2})")
+result1 = pat1.search(msg1)
+if result1:
+    print("生年月日：" + result1.group(0))
+    print("年：" + result1.group(1))
+    print("月：" + result1.group(2))
+    print("日：" + result1.group(3))
