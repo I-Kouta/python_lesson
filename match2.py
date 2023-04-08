a2d6616185c0d04c86cdf0a11132b4fd95ba12ea
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-08 11:30:58 +0900

    任意の一文字を含む場合の記述

--- match2.py
+++ match2.py
@@ -2,4 +2,10 @@
 import re
 pat1 = re.compile(r"Sun")
 print(bool(pat1.search("Sunset")))  # 日没,true
-print(bool(pat1.search("sunglasses")))  # サングラス,false
+print(bool(pat1.search("sunglasses")), "\n")  # サングラス,false
+
+pat2 = re.compile(r"s.n")
+pat2_0 = re.compile(r"s..n")
+print(bool(pat2.search("sunset")))  # .(ドット)が任意の一文字に相当する,true
+print(bool(pat2.search("soon")))  # false
+print(bool(pat2_0.search("soon")))  # true
