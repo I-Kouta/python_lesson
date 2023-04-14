94d682f37c24c90c55ed24c61e94e545346be8c8
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-14 14:40:47 +0900

    timeクラスの利用

--- date.py
+++ date.py
@@ -10,3 +10,5 @@ dt3 = datetime.datetime(2022, 4, 13, 13, 22, 31, 551, tokyo_tz)
 print(dt3)  # 2022-04-13 13:22:31.000551+10:00
 dt4 = datetime.date(2022, 4, 14)
 print(dt4)  # 2022-04-14,日付まで
+t1 = datetime.time(14, 32)
+print(t1)  # 04:14:00
