e23de4d5ae83d853b00960c09dc90880e9494eba
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-15 13:25:13 +0900

    コラッツ予想(経過の記述あり)

--- gameWhile.py
+++ gameWhile.py
@@ -33,3 +33,19 @@ else:
 # コラッツ予想
 # 1.偶数は半分にする
 # 2.奇数は3倍して1を足す
+
+
+def conjecture(n):
+    count = 0
+    while n != 1:
+        if n % 2 == 0:  # (偶数の場合)
+            n //= 2  # 奇数だと//だと切り捨て
+        else:  # (奇数の場合)
+            n = n * 3 + 1  # 3倍して1足す
+        count += 1
+        print(f"{count}回目の処理で値は{n}です")
+    print(f"初期値{initial_value}は{count}回の処理で1になります")
+
+
+initial_value = 23
+conjecture(initial_value)
