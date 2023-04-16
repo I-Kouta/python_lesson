ae0d388064e742a5dae8a97eb5183d4531006e2b
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-16 11:25:37 +0900

    現在の日付、時刻を取得する記述

--- date.py
+++ date.py
@@ -11,4 +11,8 @@ print(dt3)  # 2022-04-13 13:22:31.000551+10:00
 dt4 = datetime.date(2022, 4, 14)
 print(dt4)  # 2022-04-14,日付まで
 t1 = datetime.time(14, 32)
-print(t1)  # 04:14:00
+print(t1, "\n")  # 04:14:00
+dt5 = datetime.datetime.now(tokyo_tz)
+print(dt5)  # 今の日付と時間、()は省略可
+today = datetime.date.today()
+print(today)  # 今日
