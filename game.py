2341cdcd566c05c40cf144048e8cdb97c84ca23c
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-18 14:11:32 +0900

    リストの要素の平均値を求める関数の作成

--- game.py
+++ game.py
@@ -1,4 +1,11 @@
 # 1.リストの要素の平均値を求める
+def mean(numbers):
+    ave = sum(numbers) / len(numbers)
+    num = len(numbers)
+    print(f"{num}つの要素の平均値は{ave}です")
+
+
+mean([1, 2, 3, 4, 5])
 # 2.内積を求める
 # 3.回文かどうか判定する
 # 4.フィボナッチ数列のn番目と求める
