a107d65940ed7ad87b80222e35059c884bb61cee
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-22 11:40:48 +0900

    リストの重複を削除する記述

--- game2.py
+++ game2.py
@@ -13,8 +13,18 @@ def count_occurrences(string, target):
 
 count_occurrences("perfume", "e")
 
+# リストの重複を削除する
+# list:ただの集合をリストに変換
+# set:重複している要素を削除
+
+
+def remove_duplicates(numbers):
+    unique_numbers = list(set(numbers))
+    print(f"{numbers}から重複を除いた結果は{unique_numbers}です")
+
+
+remove_duplicates([5, 4, 3, 2, 1, 2, 3, 4, 5])
 # 指定した文字列を取り除く
-# 文字列の重複を削除する
 # 要素を逆順にする
 # 文字列を反転させる
 # 辞書型データをソートする
