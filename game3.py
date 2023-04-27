d9385d8f0ccae0172de25dd92c16736ca8e39836
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-27 14:44:46 +0900

    階乗を計算する関数

--- game3.py
+++ game3.py
@@ -1,2 +1,15 @@
 # 階乗を行う
+def factorial(n):
+    if n <= 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+m = 5
+resultFactorial = factorial(m)
+if m <= 0:
+    print(f"0以下の値が与えられています")
+else:
+    print(f"{m}の階乗は{resultFactorial}です")
 # 文字列をアルファベット準にソート
