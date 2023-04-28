5b2f945077d0057ae95e30221b3564a08d870794
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-28 14:46:06 +0900

    電卓をつくります

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# Libraries Import
+import tkinter as tk
+
+# 簡易的な電卓を作ってみた！
+
+
+class CaluGui(object):
+    def __init__(self, app=None):
+        # Window Setting
+        app.title('簡易的な電卓を作ってみた')  # Window のタイトル
+        app.geometry('400x600')  # Window のサイズ
+
+
+def main():
+    # Window Setting
+    app = tk.Tk()
+    CaluGui(app)
+
+    # Display
+    app.mainloop()  # Window をループで回すことで Widgit に対応できるようになる
+
+
+if __name__ == '__main__':
+    main()
