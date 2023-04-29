d7186e34e4b6a21f474ba2e9003ab02c91a2e788
I-Kouta <kouta.baseball0222@gmail.com>
2023-04-29 15:41:30 +0900

    フレームのセッティング

--- calculator.py
+++ calculator.py
@@ -14,9 +14,18 @@ SYMBOL = ["+", "-", "*", "/"]
 
 class CaluGui(object):  # ウィンドウ・タイトル・サイズ設定
     def __init__(self, app=None):
+        self.calc_str = ""  # 計算用の文字列
         # Window Setting
         app.title('簡易的な電卓を作ってみた')  # Window のタイトル
-        app.geometry('400x600')  # Window のサイズ
+        app.geometry('300x450')  # Window のサイズ
+
+        # frame setting
+        calc_frame = ttk.Frame(app, width=300, height=100)  # 計算式・結果用
+        calc_frame.propagate(False)  # サイズ固定
+        calc_frame.pack(side=tk.TOP, padx=10, pady=20)  # 余白設定
+        button_frame = ttk.Frame(app, width=300, height=400)  # 計算ボタンframe
+        button_frame.propagate(False)  # サイズ固定
+        button_frame.pack(side=tk.BOTTOM)  # 余白設定
 
 
 def main():
