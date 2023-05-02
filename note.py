f9cffd41b8e1d43def11a46b0f481c9925191cc2
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-02 17:11:11 +0900

    式に値を代入する

--- note.py
+++ note.py
@@ -3,4 +3,7 @@ import sympy
 
 x = sympy.Symbol("x")  # sympy.Symbol()で定義
 y = sympy.Symbol("y")
-print(type(x))
+print(type(x), "\n")
+expr = x**2 + y + 1
+print(expr)
+print(expr.subs(x, 1))  # exprにx=1を代入
