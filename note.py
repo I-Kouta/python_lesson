ca95e2989cc7cd0b6eefad825a1a398985589269
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-02 19:35:39 +0900

    subsメソッド

--- note.py
+++ note.py
@@ -6,4 +6,5 @@ y = sympy.Symbol("y")
 print(type(x), "\n")
 expr = x**2 + y + 1
 print(expr)
-print(expr.subs(x, 1))  # exprにx=1を代入
+print(f"xに1を代入:{expr.subs(x, 1)}")
+print(f"xにyを代入:{expr.subs(x, y)}")
