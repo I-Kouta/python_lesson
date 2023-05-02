64e821d8ddeb3a49d0abb3640bbf6084c268c427
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-02 19:45:39 +0900

    式の展開と因数分解のメソッド

--- note.py
+++ note.py
@@ -8,3 +8,11 @@ expr = x**2 + y + 1
 print(expr)
 print(f"xに1を代入:{expr.subs(x, 1)}")
 print(f"xにyを代入:{expr.subs(x, y)}")
+del expr
+
+expr = (x - 3) ** 2
+expr_ex = sympy.expand(expr)
+print(f"{expr} を展開：{expr_ex}")
+
+expr_factor = sympy.factor(expr_ex)
+print(f"{expr_ex} を因数分解：{expr_factor}")
