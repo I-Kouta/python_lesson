9109b963b99684adc836510fb8dd6fab3e61886e
I-Kouta <kouta.baseball0222@gmail.com>
2023-05-03 18:13:06 +0900

    三角関数・指数関数・対数関数の利用

--- note.py
+++ note.py
@@ -1,5 +1,6 @@
 # 算数・数学
 import sympy
+from sympy import sin, cos, tan, log
 
 x = sympy.Symbol("x")  # sympy.Symbol()で定義
 y = sympy.Symbol("y")
@@ -25,4 +26,15 @@ print(sympy.solve([expr1, expr2]))  # {x: ?, y: ?}
 expr3 = x**3 + 2*x**2 - x + 2
 expr3_1 = sympy.diff(expr3)
 print(f"{expr3}を微分：{expr3_1}")
-print(f"{expr3_1}を積分：{sympy.integrate(expr3_1)}")
+print(f"{expr3_1}を積分：{sympy.integrate(expr3_1)}", "\n")
+
+print(sympy.diff(sin(x)))  # cos(x)
+print(sympy.diff(cos(x)))  # -sin(x)
+print(sympy.diff(tan(x)))  # tan(x)**2 + 1
+print(sympy.diff(sympy.exp(x)))  # exp(x)
+print(sympy.diff(log(x)), "\n")  # 1/x
+print(sympy.integrate(sin(x)))  # -cos(x)
+print(sympy.integrate(cos(x)))  # sin(x)
+print(sympy.integrate(tan(x)))  # -log(cos(x))
+print(sympy.integrate(sympy.exp(x)))  # exp(x)
+print(sympy.integrate(log(x)))  # x*log(x) - x
